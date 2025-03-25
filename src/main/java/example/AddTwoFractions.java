15e6e488aeba4dedb388c9500ed916891fa435fd
Neelakantam Munagapati <mneelakantam13296@gmail.com>
2025-03-25 17:30:37 +0530

    25Mar2025 Added files

--- src/main/java/example/AddTwoFractions.java
+++ src/main/java/example/AddTwoFractions.java
@@ -2,20 +2,22 @@ package example;
 
 public class AddTwoFractions {
     public static void main(String[] args) {
-        int i= 3;
+        int a= 3;
         int b=4;
         int c=7;
         int d=3;
-        addTwoFractions(i,b,c,d);
+        addTwoFractions(a,b,c,d);
     }
 
     private static void addTwoFractions(int a, int b, int c, int d) {
         int num= (a*d) + (c*b);
         int den= b*d;
-
-
-        int sum= num/den;
-        System.out.println(num + "/"+ den);
-
+        while(num!=0){
+            int temp=num;
+            num=den%num;
+            den=temp;
+        }
+        int ans= (a*d + c*b)/den;
+        System.out.println(ans + "/"+ b*d/den);
     }
 }
